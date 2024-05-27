8c940ef7fe6a31d4b33cbe18c71fbf9a6674bef0
Augusto <augustoprados91@gmail.com>
2024-05-27 16:12:19 -0300

    mejoras

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import { Formik } from "formik";
 import { Link } from "react-router-dom";
 
 import './Main.css'
 const Main = () => {
 
+    const [isUser, setIsUser] = useState(false)
+
     const handleGoToTest = (user) => {
         const { name, lastname, age } = user;
-
         localStorage.removeItem('answers');
         localStorage.removeItem('user');
         localStorage.setItem('user', JSON.stringify({ name, lastname, age }));
-        location.replace("/test")
+        setIsUser(true)
     };
 
     return (
@@ -33,6 +35,7 @@ const Main = () => {
                 }}
 
                 onSubmit={(values, { setSubmitting }) => {
+                    setIsUser(false)
                     handleGoToTest(values)
                     setSubmitting(false);
 
@@ -52,7 +55,7 @@ const Main = () => {
                         <input className="form__input" type="text" name="lastname" onChange={handleChange} onBlur={handleBlur} value={values.lastname} placeholder="Apellido" />{errors.lastname && touched.lastname && errors.lastname}
                         <input className="form__input" type="text" name="age" onChange={handleChange} onBlur={handleBlur} value={values.age} placeholder="Edad" />{errors.age && touched.age && errors.age}
                         <div className="button-form">
-                            <Link type="submit" className='button_link' disabled={isSubmitting} to="/test"> ir al test</Link>
+                            {!isUser ? (<button type="submit" className='button_link' disabled={isSubmitting}>Guardar datos</button>) : (<Link to="/test" className='button_link'> Ir al test</Link>)}
                         </div>
                     </form>
                 )}
